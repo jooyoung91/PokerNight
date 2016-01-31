df39c2f2ad204b55daf0e49c8bfa8c2e5225ebc6
Jooyoung91 <jooyoung91@gmail.com>
2016-01-30 19:08:26 -0800

    initial files

---
+++ pokernight/PokerNight/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PokerNight.MvcApplication" Language="C#" %>
---
+++ pokernight/PokerNight/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PokerNight.Startup))]
+namespace PokerNight
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
