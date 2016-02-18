5a0855d1461009add6791baf479ff9e3ba449aa8
Jooyoung91 <jooyoung91@gmail.com>
2016-02-17 21:15:52 -0800

    add new filter js

--- pokernight/PokerNight/App_Start/BundleConfig.cs
+++ pokernight/PokerNight/App_Start/BundleConfig.cs
@@ -18,6 +18,7 @@ namespace PokerNight
                     .Include("~/Scripts/angular-min-1.2.20.js")
                     .IncludeDirectory("~/Scripts/Modules", "*.js")
                     .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                    .IncludeDirectory("~/Scripts/Utilities", "*.js")
                     .Include("~/Scripts/PokerNightApp.js"));
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
--- pokernight/PokerNight/Scripts/PokerNightApp.js
+++ pokernight/PokerNight/Scripts/PokerNightApp.js
@@ -8,15 +8,7 @@ PokerNightApp.factory('UsersFactory', UsersFactory);
 /* Controller Binding */
 PokerNightApp.controller('GameTableController', GameTableController);
 
-PokerNightApp.filter('titleCase', function () {
-    var titleCaseFilter = function (input) {
-        var words = input.split(' ');
-        for(var i= 0; i < words.length; i++)
-        {
-            words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
-        }
-        return words.join(' ');
-    }
-    return titleCaseFilter;
-});
+
+/* Filter Binding */
+PokerNightApp.filter('titleCase', DisplayFilter);
 
