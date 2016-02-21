92143fab11526d769ccfe92dad6a888dbc53881b
jooyoung91 <jooyoung91@gmail.com>
2016-02-20 22:20:16 -0800

    Changed angularjs version from 1.2.20 to 1.5.0

--- pokernight/PokerNight/Scripts/Modules/GameTableModule.js
+++ pokernight/PokerNight/Scripts/Modules/GameTableModule.js
@@ -23,28 +23,3 @@
     return tables;
 };
 
-var UsersFactory = function () {
-    var users = {};
-    users.query = function () {
-
-        return [
-            {
-                userID: 1,
-                userName: "HyungSuk",
-                score: 0
-            },
-            {
-                userID: 2,
-                userName: 'Chief91',
-                score: 200
-            },
-            {
-                userID: 3,
-                userName: 'sonuuzi',
-                score: 130
-            }
-        ]
-    }
-    return users;
-
-}
---
+++ pokernight/PokerNight/Scripts/Modules/UserModule.js
@@ -0,0 +1,25 @@
+﻿var UsersFactory = function () {
+    var users = {};
+    users.query = function () {
+
+        return [
+            {
+                userID: 1,
+                userName: "HyungSuk",
+                score: 0
+            },
+            {
+                userID: 2,
+                userName: 'Chief91',
+                score: 200
+            },
+            {
+                userID: 3,
+                userName: 'sonuuzi',
+                score: 130
+            }
+        ]
+    }
+    return users;
+
+}
